45c827bd73c0cae892ba8dc8d68420a956664afb
Stochastic <Stochasticholdings@gmail.com>
2017-03-21 11:58:48 -0400

    checkpoint-3-missing-diagram

--- app/scripts/controller/roomsCtrl.js
+++ app/scripts/controller/roomsCtrl.js
@@ -1,14 +1,22 @@
 (function() {
      function roomCtrl(Rooms) {
         var vm = this;
-        vm.rooms = [];
+        // vm.rooms = [];
 
-        Rooms.all.$loaded().then(function(){
-           angular.forEach(Rooms.all, function(value, key){
-              vm.rooms.push({id: key, data: value});
-           });
-          console.log(vm.rooms);
-        });
+        vm.rooms = Rooms.all
+
+        // Rooms.all.$loaded().then(function(){
+        //    angular.forEach(Rooms.all, function(value, key){
+        //       vm.rooms.push({id: key, data: value});
+        //    });
+        //   console.log(vm.rooms);
+        // });
+
+        vm.addRoom = function(){
+          Rooms.all.$add(vm.newRoom)
+        }
+
+        window.foo = Rooms.all
     }
 
     angular
